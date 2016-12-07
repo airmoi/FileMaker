b8e99697f82608b5a79a58351bb080a34db1d377
Romain Dunand <romain@1-more-thing.com>
2016-12-07 10:25:12 +0100

    Add DateFormat Herlper Class

---
+++ src/Helpers/DateFormat.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * FileMaker API for PHP
+ *
+ * @package FileMaker
+ *
+ * @copyright Copyright (c) 2016 by 1-more-thing (http://1-more-thing.com) All rights reserved.
+ * @license BSD
+ */
+
+namespace airmoi\FileMaker\Helpers;
+
+/**
+ * Class DateTimeHelper
+ * Provide methods to convert date input/output
+ * @package airmoi\FileMaker\Helpers
+ *
+ */
+class DateFormat
+{
+    /**
+     * @param string $value
+     * @param string $inputFormat
+     * @param string $outputFormat
+     * @return string
+     * @throws \Exception
+     */
+    public static function convert($value, $inputFormat=null, $outputFormat=null)
+    {
+        if (empty($value) || $inputFormat === null || $outputFormat === null) {
+            return $value;
+        }
+
+        //Parse value to detect incorrect date format
+        $parsedDate = date_parse_from_format($inputFormat, $value);
+        if ($parsedDate['error_count'] || $parsedDate['warning_count']) {
+            throw new \Exception('invalid date format');
+        }
+
+        $date = \DateTime::createFromFormat($inputFormat, $value);
+
+        return $date->format($outputFormat);
+    }
+}
