7ba0812ca158b584b851f982b8035900dc804e13
Romain Dunand <romain@1-more-thing.com>
2019-12-18 16:34:49 +0100

    fix wrong pagination

--- src/Command/Find.php
+++ src/Command/Find.php
@@ -104,7 +104,7 @@ class Find extends Command
 
         $pages = $result->getFoundSetCount()/100;
         for ($i = 1 ; $i < $pages; $i++) {
-            $this->setRange(($i-1)*100, 100);
+            $this->setRange($i*100, 100);
             $pageResult = $this->execute($result);
         }
         $result->fetchCount = $result->getFoundSetCount();
